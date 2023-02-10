06014bcb85edbef08088b07d068be6fa6735e21b
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-02-10 20:20:09 +0300

    build: Create empty Spring project

---
+++ src/main/java/ru/zalimannard/dripchip/DripChipApplication.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.dripchip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripChipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DripChipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/zalimannard/dripchip/DripChipApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.dripchip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripChipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
