68a4031bddd5663790bfe5e0746de186f0d80778
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-03-04 19:42:24 +0300

    feat: Create exception for 409-CONFLICT

---
+++ src/main/java/ru/zalimannard/dripchip/exception/ConflictException.java
@@ -0,0 +1,13 @@
+package ru.zalimannard.dripchip.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ConflictException extends RuntimeException {
+
+    public ConflictException() {
+        super();
+    }
+
+}
