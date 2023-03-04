395f1f3271eb8e9d262be93a179d98abc243e26d
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-03-04 16:39:47 +0300

    feat: Create Basic GET/id for Location

---
+++ src/main/java/ru/zalimannard/dripchip/location/Location.java
@@ -0,0 +1,22 @@
+package ru.zalimannard.dripchip.location;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "locations")
+@Data
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+}
