b15c008be09e644652aeba87c8354c1940e21e9c
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-04-05 20:53:28 +0300

    ref: Redesign service layer of Animal

--- src/main/java/ru/zalimannard/dripchip/schema/animal/AnimalService.java
+++ src/main/java/ru/zalimannard/dripchip/schema/animal/AnimalService.java
@@ -11,12 +11,22 @@ public interface AnimalService {
 
     AnimalDto create(@Valid AnimalDto animalDto);
 
+    Animal createEntity(@Valid Animal animal);
+
+
     AnimalDto read(@Positive long id);
 
-    List<AnimalDto> search(AnimalDto filter, Date start, Date end, @PositiveOrZero int from,
-                           @Positive int size);
+    Animal readEntity(@Positive long id);
+
+    List<AnimalDto> search(AnimalDto filterDto, Date start, Date end, @PositiveOrZero int from, @Positive int size);
+
+    List<Animal> searchEntities(Animal filter, Date start, Date end, @PositiveOrZero int from, @Positive int size);
+
 
     AnimalDto update(@Positive long id, @Valid AnimalDto animalDto);
+
+    Animal updateEntity(@Positive long id, @Valid Animal animal);
+
 
     void delete(@Positive long id);
 
--- src/main/java/ru/zalimannard/dripchip/schema/animal/AnimalServiceImpl.java
+++ src/main/java/ru/zalimannard/dripchip/schema/animal/AnimalServiceImpl.java
@@ -10,7 +10,6 @@ import ru.zalimannard.dripchip.exception.ConflictException;
 import ru.zalimannard.dripchip.exception.NotFoundException;
 import ru.zalimannard.dripchip.page.OffsetBasedPage;
 import ru.zalimannard.dripchip.schema.animal.lifestatus.AnimalLifeStatus;
-import ru.zalimannard.dripchip.schema.animal.visitedlocation.VisitedLocation;
 import ru.zalimannard.dripchip.schema.location.Location;
 
 import java.time.Instant;
@@ -22,92 +21,104 @@ import java.util.List;
 @Validated
 public class AnimalServiceImpl implements AnimalService {
 
-    private final AnimalRepository animalRepository;
     private final AnimalMapper animalMapper;
+    private final AnimalRepository animalRepository;
 
     @Override
     public AnimalDto create(AnimalDto animalDto) {
         Animal animalRequest = animalMapper.toEntity(animalDto);
-        if (animalRequest.getAnimalTypes().isEmpty()) {
-            throw new BadRequestException("Animal types should not be empty");
-        }
-        animalRequest.setLifeStatus(AnimalLifeStatus.ALIVE);
-        animalRequest.setChippingDateTime(Date.from(Instant.now()));
 
-        Animal animalResponse = saveToDatabase(animalRequest);
+        Animal animalResponse = createEntity(animalRequest);
+
         return animalMapper.toDto(animalResponse);
     }
 
     @Override
-    public AnimalDto read(long id) {
-        Animal animal = readEntity(id);
-        return animalMapper.toDto(animal);
-    }
+    public Animal createEntity(Animal animal) {
+        if (animal.getAnimalTypes().isEmpty()) {
+            throw new BadRequestException();
+        }
+        animal.setLifeStatus(AnimalLifeStatus.ALIVE);
+        animal.setChippingDateTime(Date.from(Instant.now()));
 
-    private Animal readEntity(long id) {
-        checkExist(id);
-        return animalRepository.findById(id).get();
+        return saveToDatabase(animal);
     }
 
     @Override
-    public List<AnimalDto> search(AnimalDto filterDto, Date start, Date end, int from,
-                                  int size) {
-        Animal filter = animalMapper.toEntity(filterDto);
-        Pageable pageable = new OffsetBasedPage(from, size);
-        List<Animal> animalList = animalRepository.search(start, end, filter.getChipper(),
-                filter.getChippingLocation(), filter.getLifeStatus(), filter.getGender(), pageable);
+    public AnimalDto read(long id) {
+        Animal animalResponse = readEntity(id);
 
-        return animalMapper.toDtoList(animalList);
+        return animalMapper.toDto(animalResponse);
+    }
+
+    @Override
+    public Animal readEntity(long id) {
+        return animalRepository.findById(id).orElseThrow(NotFoundException::new);
+    }
+
+    @Override
+    public List<AnimalDto> search(AnimalDto filterDto, Date start, Date end, int from, int size) {
+        Animal filter = animalMapper.toEntity(filterDto);
+
+        List<Animal> animals = searchEntities(filter, start, end, from, size);
+
+        return animalMapper.toDtoList(animals);
+    }
+
+    @Override
+    public List<Animal> searchEntities(Animal filter, Date start, Date end, int from, int size) {
+        Pageable pageable = new OffsetBasedPage(from, size);
+
+        return animalRepository.search(
+                start,
+                end,
+                filter.getChipper(),
+                filter.getChippingLocation(),
+                filter.getLifeStatus(),
+                filter.getGender(),
+                pageable
+        );
     }
 
     @Override
     public AnimalDto update(long id, AnimalDto animalDto) {
-        AnimalDto animalFromDatabaseDto = read(id);
-        Animal animalFromDatabase = animalMapper.toEntity(animalFromDatabaseDto);
         Animal animalRequest = animalMapper.toEntity(animalDto);
 
-        if ((animalFromDatabase.getLifeStatus().equals(AnimalLifeStatus.ALIVE)) &&
-                (animalRequest.getLifeStatus().equals(AnimalLifeStatus.DEAD))) {
-            animalFromDatabase.setDeathDateTime(Date.from(Instant.now()));
+        Animal animalResponse = updateEntity(id, animalRequest);
+
+        return animalMapper.toDto(animalResponse);
+    }
+
+    @Override
+    public Animal updateEntity(long id, Animal animal) {
+        Animal existedAnimal = readEntity(id);
+
+        // Animal has died since the last request
+        if ((existedAnimal.getLifeStatus().equals(AnimalLifeStatus.ALIVE)) &&
+                (animal.getLifeStatus().equals(AnimalLifeStatus.DEAD))) {
+            animal.setDeathDateTime(Date.from(Instant.now()));
         }
-
-        animalFromDatabase.setWeight(animalRequest.getWeight());
-        animalFromDatabase.setLength(animalRequest.getLength());
-        animalFromDatabase.setHeight(animalRequest.getHeight());
-        animalFromDatabase.setGender(animalRequest.getGender());
-        animalFromDatabase.setLifeStatus(animalRequest.getLifeStatus());
-        animalFromDatabase.setChipper(animalRequest.getChipper());
-        animalFromDatabase.setChippingLocation(animalRequest.getChippingLocation());
-
-        List<VisitedLocation> visitedLocations = readEntity(id).getVisitedLocations();
-        if (visitedLocations != null) {
-            if (visitedLocations.size() > 0) {
-                Location newChipherLocation = animalFromDatabase.getChippingLocation();
-                Location firstVisitedLocation = visitedLocations.get(0).getLocation();
-                if (newChipherLocation.equals(firstVisitedLocation)) {
-                    throw new BadRequestException("The first visited location should not coincide with the chipping " +
-                            "location");
-                }
+        // Chipping location coincides with the first visited location
+        if (!existedAnimal.getVisitedLocations().isEmpty()) {
+            Location newChipherLocation = animal.getChippingLocation();
+            Location firstVisitedLocation = existedAnimal.getVisitedLocations().get(0).getLocation();
+            if (newChipherLocation.equals(firstVisitedLocation)) {
+                throw new BadRequestException();
             }
         }
+        animal.setAnimalTypes(existedAnimal.getAnimalTypes());
+        animal.setId(id);
 
-        Animal accountResponse = saveToDatabase(animalFromDatabase);
-        return animalMapper.toDto(accountResponse);
+        return saveToDatabase(animal);
     }
 
     @Override
     public void delete(long id) {
-        checkExist(id);
         try {
-            animalRepository.deleteById(id);
+            Animal animal = readEntity(id);
+            animalRepository.delete(animal);
         } catch (DataIntegrityViolationException e) {
-            throw new BadRequestException("It is impossible to delete animal");
-        }
-    }
-
-    private void checkExist(long id) {
-        if (!animalRepository.existsById(id)) {
-            throw new NotFoundException("Animal", String.valueOf(id));
+            throw new BadRequestException();
         }
     }
 
@@ -115,7 +126,7 @@ public class AnimalServiceImpl implements AnimalService {
         try {
             return animalRepository.save(animal);
         } catch (DataIntegrityViolationException e) {
-            throw new ConflictException("Animal");
+            throw new ConflictException();
         }
     }
 
